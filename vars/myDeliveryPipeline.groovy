119a38fc36db8eddd87a52902c22e4328b99e766
TestTheTester <sinharahul51@yahoo.in>
2020-04-19 13:19:20 +0530

    Create myDeliveryPipeline.groovy

---
+++ vars/myDeliveryPipeline.groovy
@@ -0,0 +1,28 @@
+def call(body) {
+    // evaluate the body block, and collect configuration into the object
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
+
+    pipeline {
+    agent any
+    stages {
+        stage('test1') {
+            steps{
+                echo "Build number is ${env.BUILD_NUMBER}"
+                script {
+                    currentBuild.description = pipelineParams.build_description
+                    currentBuild.displayName = pipelineParams.displayName
+                }
+                sayHello pipelineParams.name
+                notify (type: 'slack', message: 'a slack notification')
+                printHello (user: 'Max', greet:'Hi')
+                script {
+                    logs.info 'Starting'
+                    logs.warning 'Nothing to do!'
+                }
+            }
+        }
+    }
+}
