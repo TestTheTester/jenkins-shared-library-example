ab9972da75333d350f835d30e2e62fbfbecce9f7
TestTheTester <sinharahul51@yahoo.in>
2020-04-22 00:12:20 +0530

    Update Constants.groovy

--- src/com/mybuilder/Constants.groovy
+++ src/com/mybuilder/Constants.groovy
@@ -3,4 +3,5 @@ package com.mybuilder
 class Constants {
   static final String SLACK_MESSAGE = "Sending Slack Notification"
   static final String EMAIL_MESSAGE = "Sending Email"
+  static final String REF_VAR = "$CHOICES\test"
 }
